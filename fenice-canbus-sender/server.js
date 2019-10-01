47e510d5bb0ec8ceb7f9deb95e48341c7133aeec
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-10-01 19:47:28 +0200

    shit

--- fenice-canbus-sender/server.js
+++ fenice-canbus-sender/server.js
@@ -8,6 +8,7 @@ const topic = config.mqtt.topic[0];
 const hostname = config.mqtt.hostname;
 const port = config.mqtt.port;
 const mqttUri = 'mqtt://' + hostname + ':' + port;
+const MILLIS = 5000;
 
 const channel = can.createRawChannel("vcan0", true);
 
@@ -18,86 +19,88 @@ console.log("RAM: " + os.totalmem() / 1048576 + " MB (total), " + os.freemem() /
 console.log("CPU: " + os.cpus()[0].speed + " MHz " + os.cpus()[0].model + "\n");
 
 function updateCANData(message) {
-    for (let i = 0; i < message.length; i = i + 3) {
 
-        const countTHR = canData.throttle.length;
-        const countBRK = canData.brake.length;
-        const countGPS = canData.gps.length - 1;
+    const countTHR = canData.throttle.length;
+    const countBRK = canData.brake.length;
+    const countGPS = canData.gps.length - 1;
 
-        let id_gathered = message[i];
-        let data1 = message[i + 1];
-        let data2 = message[i + 2];
-        let received7 = 0;
-        let received8 = 0;
+    let id_gathered = message.id;
+    const roba = message.data.toJSON().data;
+    let data1 = roba[0];
+    let data2 = roba[1];
+    console.log(data1)
+    console.log(data2)
+    let received7 = 0;
+    let received8 = 0;
 
-        let firstByte = ((data1 >> 24) & 255);
-        switch (id_gathered) {
-            case (0xAA):
-                if (firstByte == 0x01) {
-                    canData.bms_hv[0].volt = data1 & 16777215; //0xFFFFFF
-                } else if (firstByte == 0x0A) {
-                    canData.bms_hv[0].temp = (data1 >> 8) & 65535; //0xFFFF
+    let firstByte = ((data1 >> 24) & 255);
+    switch (id_gathered) {
+        case (0xAA):
+            if (firstByte == 0x01) {
+                canData.bms_hv[0].volt = data1 & 16777215; //0xFFFFFF
+            } else if (firstByte == 0x0A) {
+                canData.bms_hv[0].temp = (data1 >> 8) & 65535; //0xFFFF
+            }
+            break;
+        case (0xB0):
+            if (firstByte == 0x01 && countTHR < 20) {
+                canData.throttle.push((data1 >> 16) & 255);
+            } else if (firstByte == 0x02 && countBRK < 10) {
+                canData.brake.push((data1 >> 16) & 255);
+            }
+            break;
+        case (0xC0):
+            if (firstByte == 0x04) {
+                canData.imu_gyro.push({
+                    x: ((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255),
+                    y: (data1 & 255) * 256 + ((data1 >> 24) & 255),
+                    z: ((data2 >> 16) & 255) * 256 + ((data2 >> 8) & 255)
+                });
+            } else if (firstByte == 0x05) {
+                canData.imu_axel.push({
+                    x: ((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255),
+                    y: (data1 & 255) * 256 + ((data1 >> 24) & 255),
+                    z: ((data2 >> 16) & 255) * 256 + ((data2 >> 8) & 255)
+                });
+            } else if (firstByte == 0x02) {
+                canData.steering_wheel_encoder.push((data1 >> 16) & 255);
+            }
+            break;
+        case (0xD0):
+            if (firstByte == 0x07) {
+                canData.gps[countGPS].latitude = (((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255)) * 100000 + ((data1 & 255) * 256 + ((data2 >> 24) & 255));
+                canData.gps[countGPS].lat_o = (data2 >> 16) & 255;
+                canData.gps[countGPS].speed = (((data2 >> 8) & 255) * 256) + (data2 & 255);
+                if (received8 == 1) {
+                    received8 = 0;
+                    countGPS.push({});
+                } else {
+                    received7 = 1;
                 }
-                break;
-            case (0xB0):
-                if (firstByte == 0x01 && countTHR < 20) {
-                    canData.throttle.push((data1 >> 16) & 255);
-                } else if (firstByte == 0x02 && countBRK < 10) {
-                    canData.brake.push((data1 >> 16) & 255);
+            } else if (firstByte == 0x08) {
+                canData.gps[countGPS].longitude = (((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255)) * 100000 + ((data1 & 255) * 256 + ((data2 >> 24) & 255));
+                canData.gps[countGPS].lon_o = (data2 >> 16) & 255;
+                canData.gps[countGPS].altitude = (((data2 >> 8) & 255) * 256) + (data2 & 255);
+                if (received7 == 1) {
+                    received7 = 0;
+                    countGPS.push({});
+                } else {
+                    received8 = 1;
                 }
-                break;
-            case (0xC0):
-                if (firstByte == 0x04) {
-                    canData.imu_gyro.push({
-                        x: ((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255),
-                        y: (data1 & 255) * 256 + ((data1 >> 24) & 255),
-                        z: ((data2 >> 16) & 255) * 256 + ((data2 >> 8) & 255)
-                    });
-                } else if (firstByte == 0x05) {
-                    canData.imu_axel.push({
-                        x: ((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255),
-                        y: (data1 & 255) * 256 + ((data1 >> 24) & 255),
-                        z: ((data2 >> 16) & 255) * 256 + ((data2 >> 8) & 255)
-                    });
-                } else if (firstByte == 0x02) {
-                    canData.steering_wheel_encoder.push((data1 >> 16) & 255);
-                }
-                break;
-            case (0xD0):
-                if (firstByte == 0x07) {
-                    canData.gps[countGPS].latitude = (((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255)) * 100000 + ((data1 & 255) * 256 + ((data2 >> 24) & 255));
-                    canData.gps[countGPS].lat_o = (data2 >> 16) & 255;
-                    canData.gps[countGPS].speed = (((data2 >> 8) & 255) * 256) + (data2 & 255);
-                    if (received8 == 1) {
-                        received8 = 0;
-                        countGPS.push({});
-                    } else {
-                        received7 = 1;
-                    }
-                } else if (firstByte == 0x08) {
-                    canData.gps[countGPS].longitude = (((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255)) * 100000 + ((data1 & 255) * 256 + ((data2 >> 24) & 255));
-                    canData.gps[countGPS].lon_o = (data2 >> 16) & 255;
-                    canData.gps[countGPS].altitude = (((data2 >> 8) & 255) * 256) + (data2 & 255);
-                    if (received7 == 1) {
-                        received7 = 0;
-                        countGPS.push({});
-                    } else {
-                        received8 = 1;
-                    }
-                } else if (firstByte == 0x06) {
-                    canData.front_wheels_encoder.push(((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255));
-                }
-                break;
-            case (0xFF):
-                if (firstByte == 0x01) {
-                    canData.bms_lv[0].temp = (((data1 & 255) << 8) + ((data2 >> 24) & 255));
-                }
-                break;
-            case (0xAB):
-                canData.marker = 1;
-                break;
-        }
+            } else if (firstByte == 0x06) {
+                canData.front_wheels_encoder.push(((data1 >> 16) & 255) * 256 + ((data1 >> 8) & 255));
+            }
+            break;
+        case (0xFF):
+            if (firstByte == 0x01) {
+                canData.bms_lv[0].temp = (((data1 & 255) << 8) + ((data2 >> 24) & 255));
+            }
+            break;
+        case (0xAB):
+            canData.marker = 1;
+            break;
     }
+
 }
 
 function defaultCanData() {
@@ -127,7 +130,7 @@ client.on('connect', () => {
                 client.publish(topic, JSON.stringify(canData))
                 //INSERT DB di CanData
                 canData = defaultCanData();
-            }, 1000)
+            }, MILLIS)
         }
     });
 
@@ -141,7 +144,6 @@ client.on('offline', () => {
 // Log any message
 channel.addListener("onMessage",
     function (msg) {
-        console.log(msg);
         updateCANData(msg);
     }
 );
