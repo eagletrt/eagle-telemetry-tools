da12ced24d73d3c568be2cbe482b667abb5e385e
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-10-28 16:38:51 +0100

    can.sh

--- canbus-sender/tools/can.sh
+++ canbus-sender/tools/can.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-
+if [ $1 = "vcan0" ]
+    then
         echo "setting up vcan ..."
-        modprobe vcan
-        ip link add dev vcan0 type vcan
-        ifconfig vcan0 up
+        sudo modprobe vcan
+        sudo ip link add dev vcan0 type vcan
+        sudo ifconfig vcan0 up
         echo "setup done :)"
-
+    else
+        echo "setting up can0 ..."
+        sudo modprobe i2c-dev
+        sudo modprobe can_dev
+        sudo modprobe can_raw
+        sudo modprobe can
+        sudo ip link set can0 up type can bitrate 1000000
+        sudo ifconfig can0 up
+        echo "setup done :)"
+fi
